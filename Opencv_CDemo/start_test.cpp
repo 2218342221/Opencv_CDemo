3230835b81c4ea959a2513b1d5cba335e8214acd
Shawn <2218342221@qq.com>
2020-07-03 04:37:05 +0800

    nothing important

--- Opencv_CDemo/start_test.cpp
+++ Opencv_CDemo/start_test.cpp
@@ -6,7 +6,7 @@ using namespace cv; //opencv 的命名空间
 
 void opencvVS_helloWorld() {
     printf("Hello Open CV!");
-    Mat img = imread("resources/lena.jpg");  //这个图片需要自己准备，放在project目录下，或者直接写绝对路径
+    Mat img = imread("resources/lena.jpg");  //因为 调试->工作目录 就是在项目目录下，所以这里可以写相对路径。
     imshow("lena", img);//显示图片6秒
     waitKey(6000);
 }
